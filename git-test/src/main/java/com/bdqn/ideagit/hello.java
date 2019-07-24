308b01576681ce6f989ef7d21a8552cd1cfc00e9
syy <1498537314@qq.com>
2019-07-24 14:27:12 +0800

    修改hello.java

--- git-test/src/main/java/com/bdqn/ideagit/hello.java
+++ git-test/src/main/java/com/bdqn/ideagit/hello.java
@@ -3,6 +3,7 @@ package com.bdqn.ideagit;
 public class hello {
     public void hello(){
         System.out.println("你好");
-        System.out.println("你reps1");
+        System.out.println("你hello");
+        System.out.println("你修改hello");
     }
 }
