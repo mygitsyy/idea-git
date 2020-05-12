945c7b321995acfcf2e368ae545ab7828a378673
syy <1498537314@qq.com>
2020-05-12 16:01:05 +0800

    暂存hello

--- git-test/src/main/java/com/bdqn/ideagit/hello.java
+++ git-test/src/main/java/com/bdqn/ideagit/hello.java
@@ -1,7 +1,7 @@
 package com.bdqn.ideagit;
 
 public class hello {
-    public void hello(){
+    public void hello() {
         System.out.println("你好");
         System.out.println("你reps1");
         System.out.println("添加reps1");
@@ -10,6 +10,8 @@ public class hello {
         System.out.println("操作人reps1");
         System.out.println("sjdhg");
         System.out.println("操作人reps2");
+        System.out.println("操作人reps1sfbg....");
+        System.out.println("操作人re159458945");
         System.out.println("操作人reps1sfbg....");
         System.out.println("操作人re159458945");
     }
